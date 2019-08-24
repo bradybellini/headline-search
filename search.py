aadcccadf466a4e4d26a20e5d5f91a9de30e2212
brady <bellinibrady65@gmail.com>
2019-08-24 00:04:50 -0700

    testing things

--- search.py
+++ search.py
@@ -1,1 +1,8 @@
 import feedparser
+from feeds import rss_feeds
+
+# feeds = ['https://kotaku.com/rss', 'https://www.pcgamer.com/rss']
+for feed in rss_feeds:
+    f = feedparser.parse(feed)
+    [print(entry['title']) for entry in f.entries if 'Games' in entry['title']]
+# print(f.entries[0]['title'])

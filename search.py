d2024cb92e72d4c9ec78033860d7b7c6e9dfef0f
brady <bellinibrady65@gmail.com>
2019-08-24 14:39:39 -0700

    guid is now unique in db and try except checks that

--- search.py
+++ search.py
@@ -3,7 +3,6 @@ import sqlite3
 import datetime
 from feeds import rss_feeds
 
-# feeds = ['https://kotaku.com/rss', 'https://www.pcgamer.com/rss']
 def main():
     for feed in rss_feeds:
         f = feedparser.parse(feed)
@@ -13,15 +12,12 @@ def main():
             date_added = datetime.datetime.now()
             main = sqlite3.connect('main.db')
             cursor = main.cursor()
-            cursor.execute("SELECT guid FROM headlines")
-            guid_dup = cursor.fetchall()
-            for guid in guid_dup:
-                if entry['id'] in guid[0]:
-                    pass
-                else:
-                    sql = (f"INSERT INTO headlines(title, link, author, feed, date_published, date_added, guid) VALUES(?,?,?,?,?,?,?)")
-                    val = (entry['title'], entry['link'], author, feed, published, date_added, entry['id'])
-                    cursor.execute(sql, val)
+            sql = (f"INSERT INTO headlines(title, link, author, feed, date_published, date_added, guid) VALUES(?,?,?,?,?,?,?)")
+            val = (entry['title'], entry['link'], author, feed, published, date_added, entry['id'])
+            try:
+                cursor.execute(sql, val)
+            except:
+                pass
             main.commit()
             cursor.close()
             main.close()

063639e82f23c44475f859ae0f3d54eaa92e866e
brady <bellinibrady65@gmail.com>
2019-08-25 00:19:54 -0700

    messing around

--- search.py
+++ search.py
@@ -1,19 +1,26 @@
 import feedparser
 import sqlite3
 import datetime
-from feeds import rss_feeds
+import time
+from feeds import all_feeds
 
-def news():
-    for feed in rss_feeds:
+def main():
+    for feed in all_feeds:
         f = feedparser.parse(feed)
         for entry in f.entries:
             author = entry['author'] if 'author' in entry else None
-            published = entry['published'] if 'published' in entry else None
+            try:
+                published = time.asctime(entry.published_parsed) if 'published' in entry else None
+            except:
+                published = entry.published if 'published' in entry else None
+            link = entry['link'] if 'link' in entry else None
+            guid = entry['id'] if 'id' in entry else None
+            title = entry['title'] if 'title' in entry else None
             date_added = datetime.datetime.now()
             news = sqlite3.connect('news.db')
             cursor = news.cursor()
             sql = (f"INSERT INTO headlines(title, link, author, feed, date_published, date_added, guid) VALUES(?,?,?,?,?,?,?)")
-            val = (entry['title'], entry['link'], author, feed, published, date_added, entry['id'])
+            val = (title, link, author, feed, published, date_added, guid)
             try:
                 cursor.execute(sql, val)
             except:
@@ -22,6 +29,8 @@ def news():
             cursor.close()
             news.close()
 # print(f.entries[0]['title'])
-
-if __name__ == "__news__":
-    news()
+#TODO check if link to article works if not try guid if not dont put it in database
+#change date_added to be the same format as published date
+#check for an id another way. Some feeds do not have a guid i.e. tech hive, pcworld feed, macworld, metacritic, sports 24
+if __name__ == "__main__":
+    main()

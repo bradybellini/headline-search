8fd3b48798ef34b751461c85f06a7fabea31a53e
brady <bellinibrady65@gmail.com>
2019-08-25 17:42:37 -0700

    Cleaned up and added exceptions where needed

--- search.py
+++ search.py
@@ -2,45 +2,82 @@ import feedparser
 import sqlite3
 import datetime
 import time
-from feeds import all_feeds
+from feeds import *
 
-def main():
-    for feed in all_feeds:
+def get_entry():
+    for feed in movies_feeds:
         f = feedparser.parse(feed)
         for entry in f.entries:
-            author = entry['author'] if 'author' in entry else None
-            try:
-                published = time.asctime(entry.published_parsed) if 'published' in entry else None
-            except:
-                published = entry.published if 'published' in entry else None
-            link = entry['link'] if 'link' in entry else None
-            guid = entry['id'] if 'id' in entry else None
-            title = entry['title'] if 'title' in entry else None
-            date_added = datetime.datetime.now()
-            news = sqlite3.connect('news.db')
-            cursor = news.cursor()
-            sql = (f"INSERT INTO headlines(title, link, author, feed, date_published, date_added, guid) VALUES(?,?,?,?,?,?,?)")
-            val = (title, link, author, feed, published, date_added, guid)
-            try:
-                cursor.execute(sql, val)
-            except:
+            mandatory_elements(entry)
+    return feed
+
+
+def mandatory_elements(entry):
+    try:
+        if not 'id' in entry:
+            guid = entry.link + entry.author
+            parse_entry(entry, guid)
+        elif not 'published' in entry:
+            pass
+        else:
+            guid = entry.guid
+            parse_entry(entry, guid)
+    except:
+        try:
+            if not 'id' in entry or not 'published' in entry:
                 pass
-            news.commit()
-            cursor.close()
-            news.close()
-# print(f.entries[0]['title'])
+            else:
+                guid = entry.guid
+                parse_entry(entry, guid)
+        except:
+            pass
+
+
+def parse_entry(entry, guid):
+    author = entry.author if 'author' in entry else 'No author provided'
+    date_published = date_format(entry)
+    title = entry.title
+    date_added = time.time()
+    link = entry.link
+    insert_data(title, link, author, date_published, date_added, guid)
+
+
+def date_format(entry):
+    try:
+        published = time.mktime(entry.published_parsed)
+        return published
+    except:
+        pass
+
+
+def insert_data(title, link, author, date_published, date_added, guid):
+    news = sqlite3.connect('news.db')
+    cursor = news.cursor()
+    sql = (f"INSERT INTO headlines(title, link, author, date_published, date_added, guid) VALUES(?,?,?,?,?,?)")
+    val = (title, link, author, date_published, date_added, guid)
+    try:
+        cursor.execute(sql,val)
+    except:
+        print('error in instert_data')
+        pass
+    news.commit()
+    cursor.close()
+    news.close()
+
+
+if __name__ == "__main__":
+    get_entry()
+
 #TODO check if link to article works if not try guid if not dont put it in database
-#change date_added to be the same format as published date also change date for published to only be numbers for sorting purposes. it is not sorting right in database
-#check for an id another way. Some feeds do not have a guid i.e. tech hive, pcworld feed, macworld, metacritic, sports 24
 #add all if else, try expect to handle all errors and make all data uniform so it is easier to parse in database
 #check for etag and modified for rss feeds, if the feed has it, to limit bandwidth and ping to rss feed
-#make function above into class
+#make functions above into class
 #count occurences of certain words in a period of time to see what is trending. Might need to use nltk or something to get nound and subjects in headlines
+#graph occurence vs time and take derivative to help gauge trendyness
 #think about parsing summaries, descriptions for nounds for trending
 #add better search for discord bot
 #think about adding more tables for each category and test speeds for searching.
 #add tags if they exists and category the article is in
 #switch to using json doc with all feeds and site
 #add source to result, i.e. kotaku.com is kotaku cnn.com is cnn
-if __name__ == "__main__":
-    main()
+
--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
 import feedparser
 import requests
 import json
+import time
 from feeds import *
 
 # game_feeds = ['https://kotaku.com/rss', 'https://www.usgamer.net/rss']
@@ -8,61 +9,61 @@ from feeds import *
 # r = requests.get('https://kotaku.com/rss')
 # rr = r.headers.get('Etag')
 # feed_list = [game_feeds, worldnews_feeds, usnews_feeds, technology_feeds, movies_feeds, sports_feeds, entertainment_feeds]
-feeds= {}
-feeds['gaming'] = []
-feeds['general'] = []
-feeds['technology'] = []
-feeds['politics'] = []
-feeds['movies'] = []
-feeds['sports'] = []
-feeds['entertainment'] = []
+# feeds= {}
+# feeds['gaming'] = []
+# feeds['general'] = []
+# feeds['technology'] = []
+# feeds['politics'] = []
+# feeds['movies'] = []
+# feeds['sports'] = []
+# feeds['entertainment'] = []
 
 # , usnews, technology, movies, sports, entertainment
 # , usnews_feeds, technology_feeds, movies_feeds, sports_feeds, entertainment_feeds
-for game in game_feeds:
-    f = feedparser.parse(game)
-    feeds['gaming'].append({
-                    'name': f.feed.title,
-                    'website_url': f.feed.link,
-                    'feed': game})
-for general in generalnews_feeds:
-    f = feedparser.parse(general)
-    feeds['general'].append({
-                'name': f.feed.title,
-                'website_url': f.feed.link,
-                'feed': general})
-for technology in technology_feeds:
-    f = feedparser.parse(technology)
-    feeds['technology'].append({
-                'name': f.feed.title,
-                'website_url': f.feed.link,
-                'feed': technology})
-for politics in politics_feeds:
-    f = feedparser.parse(politics)
-    feeds['politics'].append({
-                'name': f.feed.title,
-                'website_url': f.feed.link,
-                'feed': politics})
-for movies in movies_feeds:
-    f = feedparser.parse(movies)
-    feeds['movies'].append({
-                'name': f.feed.title,
-                'website_url': f.feed.link,
-                'feed': movies})
-for sports in sports_feeds:
-    f = feedparser.parse(sports)
-    feeds['sports'].append({
-                'name': f.feed.title,
-                'website_url': f.feed.link,
-                'feed': sports})
-for entertainment in entertainment_feeds:
-    f = feedparser.parse(entertainment)
-    feeds['entertainment'].append({
-                'name': f.feed.title,
-                'website_url': f.feed.link,
-                'feed': entertainment})
-with open('feeds_2.json', 'w') as a:
-    json.dump(feeds, a)
+# for game in game_feeds:
+#     f = feedparser.parse(game)
+#     feeds['gaming'].append({
+#                     'name': f.feed.title,
+#                     'website_url': f.feed.link,
+#                     'feed': game})
+# for general in generalnews_feeds:
+#     f = feedparser.parse(general)
+#     feeds['general'].append({
+#                 'name': f.feed.title,
+#                 'website_url': f.feed.link,
+#                 'feed': general})
+# for technology in technology_feeds:
+#     f = feedparser.parse(technology)
+#     feeds['technology'].append({
+#                 'name': f.feed.title,
+#                 'website_url': f.feed.link,
+#                 'feed': technology})
+# for politics in politics_feeds:
+#     f = feedparser.parse(politics)
+#     feeds['politics'].append({
+#                 'name': f.feed.title,
+#                 'website_url': f.feed.link,
+#                 'feed': politics})
+# for movies in movies_feeds:
+#     f = feedparser.parse(movies)
+#     feeds['movies'].append({
+#                 'name': f.feed.title,
+#                 'website_url': f.feed.link,
+#                 'feed': movies})
+# for sports in sports_feeds:
+#     f = feedparser.parse(sports)
+#     feeds['sports'].append({
+#                 'name': f.feed.title,
+#                 'website_url': f.feed.link,
+#                 'feed': sports})
+# for entertainment in entertainment_feeds:
+#     f = feedparser.parse(entertainment)
+#     feeds['entertainment'].append({
+#                 'name': f.feed.title,
+#                 'website_url': f.feed.link,
+#                 'feed': entertainment})
+# with open('feeds_2.json', 'w') as a:
+#     json.dump(feeds, a)
 
 # ff = feedparser.parse('https://www.cnet.com/rss/gaming/', modified=f.modified)
 # print(ff.status)
@@ -73,3 +74,8 @@ with open('feeds_2.json', 'w') as a:
 #     f = json.load(read_file)
 # for name in f['gaming']:
 #     print(name['name'])
+
+
+f = feedparser.parse("https://www.gamesindustry.biz/rss/gamesindustry_news_feed.rss")
+time_1 = time.mktime(f.entries[2].published_parsed)
+print(time.time())

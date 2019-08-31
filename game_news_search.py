eae82098f8499578b3fc74d71e20ec0e3a12fe1f
bradybellini <bellinibrady65@gmail.com>
2019-08-31 15:18:33 -0700

    not working need to fix

--- game_news_search.py
+++ game_news_search.py
@@ -1,18 +1,24 @@
 import feedparser
 import sqlite3
-import datetime
 import time
 import nltk
+import json
 from nltk.corpus import stopwords
 from feeds import game_feeds
 
 
+# def get_entry():
+#     with open('feeds.json', 'r') as read_file:
+#         f = json.load(read_file)
+#     for feed in f['gaming']:
+#         f = feedparser.parse(feed['feed'])
+#         for entry in f.entries:
+#             mandatory_elements(entry)
 def get_entry():
     for feed in game_feeds:
         f = feedparser.parse(feed)
         for entry in f.entries:
             mandatory_elements(entry)
-
 
 def mandatory_elements(entry):
     try:
@@ -33,21 +39,25 @@ def mandatory_elements(entry):
                 insert_data(entry=entry, guid=guid)
         except:
             pass
+            print('all failed in mand')
 
 
 def date_format(entry):
     try:
         published = time.mktime(entry.published_parsed)
         insert_data(date_published=published)
+        print('date sucessful')
     except:
         print('error in date_format')
         pass
-
+    print('date ran')
 
 def get_tags(entry):
+    print('tags ran')
     try:
         tags = entry.tag['term']
         insert_data(tags=tags)
+        print('tags sucessful')
     except:
         stop_words = set(stopwords.words('english')) 
         all_nouns = [word for (word, pos) in nltk.pos_tag(nltk.word_tokenize(entry.title)) if pos[0] == 'N']
@@ -58,20 +68,23 @@ def get_tags(entry):
         tags = ', '.join(tags)
         insert_data(tags=tags)
         print('error in tags')
-    return tags
+
+
 
 
 def insert_data(**kwargs):
+    get_tags(kwargs.get('entry'))
     author = kwargs.get('entry').author if 'author' in kwargs.get('entry') else 'No author provided'
     game_news = sqlite3.connect('game_news.db')
     cursor = game_news.cursor()
+    print(kwargs.get('tags'))
     sql = (f"INSERT INTO news(title, link, author, tags, date_published, date_added, guid) VALUES(?,?,?,?,?,?,?)")
     val = (kwargs.get('entry').title, kwargs.get('entry').link, author, kwargs.get('tags'), kwargs.get('date_published'), time.time(), kwargs.get('guid'))
     try:
         cursor.execute(sql,val)
     except:
         pass
-    print('error in insert_data')
+        print('error in insert_data')
     game_news.commit()
     cursor.close()
     game_news.close()
@@ -89,6 +102,5 @@ if __name__ == "__main__":
 #think about parsing summaries, descriptions for nouns for trending
 #add better search for discord bot
 #think about adding more tables for each category and test speeds for searching.
-#switch to using json doc with all feeds and site
 #add source to result, i.e. kotaku.com is kotaku cnn.com is cnn
 

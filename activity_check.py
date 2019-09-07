b40f3f9e86a0e1bcd2fe0ae2393f108a1d588843
bradybellini <bellinibrady65@gmail.com>
2019-09-06 21:50:41 -0700

    preliminary testing for headers

--- activity_check.py
+++ activity_check.py
@@ -1,5 +1,17 @@
 import feedparser
 import json
 import time
+import requests
 
 #check if match or etag for feed or pass if neither
+r = requests.get('https://www.cnet.com/rss/gaming/')
+print(r.headers)
+
+def has_feed_etag():
+    pass
+
+def has_feed_mod():
+    pass
+
+def has_feed_none():
+    pass

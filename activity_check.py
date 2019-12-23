a8620d75d1efffdb602f06c5555d1439b9ded10c
Brady <bellinibrady65@gmail.com>
2019-12-23 10:18:23 -0700

    Update activity_check.py

--- activity_check.py
+++ activity_check.py
@@ -1,17 +1,4 @@
-import feedparser
-import json
-import time
-import requests
+import itertools
 
-#check if match or etag for feed or pass if neither
-r = requests.get('https://www.cnet.com/rss/gaming/')
-print(r.headers)
-
-def has_feed_etag():
-    pass
-
-def has_feed_mod():
-    pass
-
-def has_feed_none():
-    pass
+a = itertools.product([True,False], repeat=2)
+print(a)
